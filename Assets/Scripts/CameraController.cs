bf2b6445a85dfc80a6a6f964728c65d7e255a6b9
Saucyz <richardgleong@gmail.com>
2016-12-15 19:18:32 -0800

    Camera follows player ball without rotating with the ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// For follow cameras, late update runs every frame after all items processed, like position
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
