a39a9cec663ef6495312d5b42524698b2ae3acf4
Saucyz <richardgleong@gmail.com>
2016-12-15 23:41:08 -0800

    UI text to display + count pick ups and win text on collecting all 12

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI; // Need to interacti with UI
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb; // Holds reference for rigidbody on object
     public float speed = 10; // Default speed, can be changed in editor
+    private int count;
+    public Text countText;
+    public Text winText;
+
     // First frame script is active
     void Start() {
+        count = 0;
         rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = "";
     }
 
     // Called before physics calculations
@@ -24,6 +32,18 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.CompareTag("Pick Up")) // Label this tag in Unity
         {
             other.gameObject.SetActive(false); // disable the game object in scene, not deleting
+            count++;
+            SetCountText();
+        }
+    }
+
+    // Updates UI text to current count of pick ups
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
