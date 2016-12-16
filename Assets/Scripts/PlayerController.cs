d3e075680f6823da5bece42463660056af97df96
Saucyz <richardgleong@gmail.com>
2016-12-15 16:50:49 -0800

    Player movement of ball based on arrow keys

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb; // Holds reference for rigidbody on object
+    public float speed = 10; // Default speed, can be changed in editor
+    // First frame script is active
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Called before physics calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical); //Ball should roll on axis, no y movement
+
+        rb.AddForce(movement * speed); // Adds force in direction of Vector3 from input sped up by speed
+    }
+}
