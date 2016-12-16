736d395a6dc6c5dfac27803ae091ce4b14dc8ec4
Saucyz <richardgleong@gmail.com>
2016-12-15 23:21:27 -0800

    Pick up objects with correct collider detection and physics, deactivate on touching player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,25 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb; // Holds reference for rigidbody on object
     public float speed = 10; // Default speed, can be changed in editor
     // First frame script is active
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
     }
 
     // Called before physics calculations
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical); //Ball should roll on axis, no y movement
 
         rb.AddForce(movement * speed); // Adds force in direction of Vector3 from input sped up by speed
+    }
+
+    // OnTriggerEnter is called by Unity when touching trigger collider, other is reference to collider we touched
+    // Trigger colliders won't use physics
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Pick Up")) // Label this tag in Unity
+        {
+            other.gameObject.SetActive(false); // disable the game object in scene, not deleting
+        }
     }
 }
