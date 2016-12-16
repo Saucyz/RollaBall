b7e171ca122c164a60e0d289433b3e3ad9e88c72
Saucyz <richardgleong@gmail.com>
2016-12-15 23:00:29 -0800

    Pick up objects which rotate are in

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime); // Multiplying this smoothens by multiplying rotation by time it took for last frame, framerate independence
+	}
+}
